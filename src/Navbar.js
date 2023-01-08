6106991da4788ee4ab66ec8c30cc31b6475fa73c
inkvoiid <kalebdickson@live.com>
2023-01-08 20:14:02 +1300

    Changed anchor tags to React Link tags

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from './media/images/thevoiid.png';
 
 function Navbar(){
@@ -6,10 +7,10 @@ function Navbar(){
 		<nav className="m-3">
 			<a href="https://inkvoiid.netlify.app"><img src={Logo} alt="the voiid." /></a>
 			<ul>
-			  	<li><a id="currentPage" href="/">&gt;Home</a></li>
-			  	<li><a href="/voiidpack">&gt;voopack</a></li>
-			  	<li><a href="/photography">&gt;Photography</a></li>
-			  	<li><a href="/music">&gt;Music</a></li>
+			  	<li><Link to="/">&gt;Home</Link></li>
+			  	<li><Link to="/voiidpack">&gt;voopack</Link></li>
+			  	<li><Link to="/photography">&gt;Photography</Link></li>
+			  	<li><Link to="/music">&gt;Music</Link></li>
 			</ul>
 		</nav>
 		<a class="twitter-timeline" data-theme="dark" href="https://twitter.com/inkvoiid?ref_src=twsrc%5Etfw">Tweets by inkvoiid</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
