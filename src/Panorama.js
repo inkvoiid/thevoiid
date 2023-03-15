00976ec3307b180c26a47a517e02aec06c7427c1
inkvoiid <kalebdickson@live.com>
2023-03-15 23:53:40 +1300

    No Anonymous Default Exports

--- src/Panorama.js
+++ src/Panorama.js
@@ -2,10 +2,12 @@ import React, { useMemo } from "react";
 import View360, { EquirectProjection } from "@egjs/react-view360";
 import "@egjs/react-view360/css/view360.min.css";
 
-export default ({src}) => {
+const pano = ({src}) => {
   const projection = useMemo(() => new EquirectProjection({
     src
   }), [src]);
 
   return <View360 className="is-16by9" projection={projection} />
 }
+
+export default pano;
