42974ba40d96d8b7d3b3aea7f5c6f4a6c8d6ad87
inkvoiid <kalebdickson@live.com>
2023-04-11 05:09:47 +1200

    Update

--- src/routes/projects/VoiidOS.js
+++ src/routes/projects/VoiidOS.js
@@ -5,7 +5,7 @@ function VoiidOS(){
         <h1>VoiidOS</h1>
       </div>
       <div className="container">
-        <p>Trinklet 4 Zach.</p>
+        <p>Don't leave the Trinklet running!</p>
         <iframe title="The Trinklet" src="https://trinket.io/embed/python3/c57dc49098?outputOnly=true" width="100%" height="500px" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
       </div>
 	</main>
