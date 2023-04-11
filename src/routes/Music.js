3f4ba32a353886881c83a803abd88434f54e25fa
inkvoiid <kalebdickson@live.com>
2023-04-11 15:56:46 +1200

    Photography Grid

--- src/routes/Music.js
+++ src/routes/Music.js
@@ -9,17 +9,34 @@ const images = [
 function Music(){
 	return (
 	<main>
-		<div className="page-title underline">
-			<h1>Music</h1>
+			<div className="page-title underline">
+				<h1>Music</h1>
 			</div>
-		<div className="container">
-			<p>Big fan of The Beatles and Peach Pit. Own some records.</p>
-		</div>
+			
+		<article>
+			<section className="container left">
+				<p>Welcome to the Music page! This is where you can check out my <a href="#Record-Collection">Record Collection</a> or my <a href="#Music-Gear">Music Gear</a>. Big fan of The Beatles and Peach Pit.</p>
+			</section>
 
-		<div className="container container-green">
-		<h1>Record Collection</h1>
-		<div className="inner-container"><Coverflow images={images}></Coverflow></div>
-		</div>
+			<section className="container">
+				<h1>What is inkvoiid listening to?</h1>
+				<p>Your Mother Should Know - The Beatles</p>
+			</section>
+
+			<section id="Record-Collection" className="container container-green">
+				<h1>Record Collection</h1>
+				<div className="inner-container">
+					<Coverflow images={images}></Coverflow>
+				</div>
+			</section>
+
+			<section id="Music-Gear" className="container">
+				<h1>Music Gear</h1>
+				<div className="inner-container">
+					<p>Coming soon...</p>
+				</div>
+			</section>
+		</article>
 	</main>
 	  );
 }
--- src/routes/Photography.js
+++ src/routes/Photography.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import tiltJquery from "tilt.js";
+import VanillaTilt from "vanilla-tilt";
 import CameraPic from "../media/images/camera.jpg"
 
 const images = require.context("../media/images/gallery", false)
@@ -12,24 +13,43 @@ function Photography(){
 		<div className="page-title underline">
         	<h1>Photography</h1>
 		</div>
-		<article className="column-layout container">
-			<div>
-				<img className="rounded" src={CameraPic} width="100%" alt="My father's Canon EOS550D"/>
-			</div>
-			<div style={{textAlign:"center"}}>
-				<p>I garnered a passion in photography when I opted to take it as a subject in high school in 2019. Most, if not all of these photos have been taken on my father's Canon EOS550D.</p>
-				<p>The photography classes taught me all about aperture, shutter speed, leading lines and the rule of thirds. You may be able to tell I like to do some digital manipulation.</p>
-				<p>Take a look at some snaps I've done in my lifetime.</p>
-			</div>
-		</article>
+		<article>
+			<section className="column-layout container">
+				<div>
+					<img className="rounded" src={CameraPic} width="100%" alt="My father's Canon EOS550D"/>
+				</div>
+				<div className="left">
+					<p>I garnered a passion in photography when I opted to take it as a subject in high school in 2019. Most, if not all of these photos have been taken on my father's Canon EOS550D.</p>
+					<p>The photography classes taught me all about aperture, shutter speed, leading lines and the rule of thirds. You may be able to tell I like to do some digital manipulation.</p>
+					<p>Take a look at <a href="#Gallery">some snaps I've done in my lifetime</a>, some <a href="#Baking">photos of my baking</a>, or <a href="#Japan">my dad's photos from Japan</a>.</p>
+				</div>
+			</section>
+	
+			<section id="Gallery" className="container">
+				<h1>Gallery</h1>
+			</section>
+			<section className="gallery">	
+				{imagePaths.map((path, index) => (
+				<div data-tilt key={images(path)} className="polaroid">
+					<img src={images(path)} alt={images(path)}/>
+					<p>{images.keys()[index].split('/').pop().replace(/\.[^/.]+$/, "")}</p>
+				</div>))}
+			</section>
 
-		<div className="gallery">			
-			{imagePaths.map((path, index) => (
-			<div tilt className="polaroid">
-				<img src={images(path)} alt={images(path)}/>
-				<p>{images.keys()[index].split('/').pop().replace(/\.[^/.]+$/, "")}</p>
-			</div>))}
-		</div>
+			<section id="Baking" className="container">
+				<h1>Baking Photos</h1>
+				<div className="inner-container">
+					<p>Coming soon...</p>
+				</div>
+			</section>
+
+			<section id="Japan" className="container">
+				<h1>Dad's Photos from Japan</h1>
+				<div className="inner-container">
+					<p>Coming soon...</p>
+				</div>
+			</section>
+		</article>
 	</main>
 	  );
 }
