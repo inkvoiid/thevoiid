1aa98dd5d803c2b0b9da204326a4003db062ffbe
inkvoiid <kalebdickson@live.com>
2023-04-14 14:45:27 +1200

    Record Shelves

--- src/routes/partial/RecordCollection.js
+++ src/routes/partial/RecordCollection.js
@@ -63,9 +63,69 @@ function RecordCollection() {
         fetchData();
       }, []);
 
+      // Sort the filtered collection by artist name and album name
+      collection.sort((a, b) => {
+        if (a.artist < b.artist) {
+          return -1;
+        }
+        if (a.artist > b.artist) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
+
+      const beatlesRecords = [];
+
+      // Remove records from John Lennon, Paul McCartney, George Harrison, Ringo Starr, The Beatles, Traveling Wilburys, and Wings
+      const filteredCollection = collection.filter((album) => {
+        if (
+          album.artist === "John Lennon" ||
+          album.artist === "Paul McCartney" ||
+          album.artist === "George Harrison" ||
+          album.artist === "Ringo Starr" ||
+          album.artist === "The Beatles" ||
+          album.artist === "Traveling Wilburys" ||
+          album.artist === "Wings"
+        ) {
+          beatlesRecords.push(album);
+          return false;
+        }
+        return true;
+      });
+
+      const chunkSize = 25;
+      const chunkedCollection = [];
+
+      for (let i = 0; i < filteredCollection.length; i += chunkSize) {
+        chunkedCollection.push(filteredCollection.slice(i, i + chunkSize));
+      }
+  
+      return (
+        <div>
+        <h2 className='underline'>Beatles Collection</h2>
+          <RecordShelf style={{marginBlock:"1rem"}} data={beatlesRecords} />
+
+          {chunkedCollection.map((chunk, index) => (
+            
+            <div>
+            <h2 className='underline'>Shelf {index+1}</h2>
+            <RecordShelf style={{marginBlock:"1rem"}} key={index} data={chunk} />
+            </div>
+          ))}
+        </div>
+      );
+}
+
+function RecordShelf({ data }) {
   return (
-    <div className="record-collection inner-container">
-      {collection.map((album) => (
+    <div className="record-collection">
+      {data.map((album) => (
         <div className="record" key={album.id} style={{backgroundImage:`url(${album.image})`}}>
             <div className="record-info">
             <h2 className="title">{album.title}</h2>
