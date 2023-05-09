916438c1103a3a152fdc76cb704a00ef7572f2b5
inkvoiid <kalebdickson@live.com>
2023-05-09 22:43:34 +1200

    Minecraft Servers from Json

--- src/routes/MinecraftWorlds.js
+++ src/routes/MinecraftWorlds.js
@@ -1,26 +1,49 @@
+import React from "react";
 import ScrollSpy from "react-ui-scrollspy";
 import '@splidejs/react-splide/css';
 import Panorama from "./partial/Panorama";
 import "../css/minecraftworlds.css"
+
+import minecraftServers from "../data/minecraft-servers.json";
 import DamonBase from "../media/images/minecraftworlds/panoramas/damonskybase.jpg";
 import ZachHouse from "../media/images/minecraftworlds/panoramas/zachhouse.jpg";
+import ServerEntry from "./partial/ServerEntry";
 
-
+function getYearFromJsonDate(jsonDate) {
+	const date = new Date(jsonDate.split(' ').pop());
+	console.log(date)
+	const year = date.getFullYear();
+	console.log(year)
+	return year;
+  }
+  
 
 function MinecraftWorlds(){
+
+	const serversByYear = {};
+  minecraftServers.data.forEach((server) => {
+    const year = getYearFromJsonDate(server.datecreated);
+    if (!serversByYear[year]) {
+      serversByYear[year] = [];
+    }
+    serversByYear[year].push(server);
+  });
+
+  const yearHeadings = Object.keys(serversByYear).sort();
+
 	return (
 	<main>
 		<div className="scrollspyNav">
-			<ul>
-				<li><a data-to-scrollspy-id="mc2017" href="#mc2017">2017</a></li>
-				<li><a data-to-scrollspy-id="mc2018" href="#mc2018">2018</a></li>
-				<li><a data-to-scrollspy-id="mc2019" href="#mc2019">2019</a></li>
-				<li><a data-to-scrollspy-id="mc2020" href="#mc2020">2020</a></li>
-				<li><a data-to-scrollspy-id="mc2021" href="#mc2021">2021</a></li>
-				<li><a data-to-scrollspy-id="mc2022" href="#mc2022">2022</a></li>
-				<li><a data-to-scrollspy-id="mc2023" href="#mc2023">2023</a></li>
-			</ul>
-		</div>
+        <ul>
+          {yearHeadings.map((year) => (
+            <li key={year}>
+              <a data-to-scrollspy-id={`mc${year}`} href={`#mc${year}`}>
+                {year}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
 
 		<div className="page-title underline">
         	<h1>Minecraft Worlds</h1>
@@ -29,43 +52,28 @@ function MinecraftWorlds(){
 		<section className="container">
 			<p>View some of the Minecraft server worlds I've catalogued. Who knows? Perhaps you might have taken part in some of them.</p>
 			<h2>Filters</h2>
-			<p><span>DumbleDog_</span> <span>inkvoiid</span> <span>WispyDutchess</span></p>
+			<p>
+				<span className="playertag">DumbleDog_</span> 
+				<span className="playertag">inkvoiid</span> 
+				<span className="playertag">WispyDutchess</span>
+			</p>
 		</section>
 		<ScrollSpy>
-			<h1 id="mc2017" className="underline" style={{textAlign:"center",color:"white", textDecorationColor:"steelblue"}}>2017</h1>
-
-			<h1 id="mc2018" style={{textAlign:"center",color:"white"}}>2018</h1>
-
-			<h1 id="mc2019" style={{textAlign:"center",color:"white"}}>2019</h1>
-
-			<h1 id="mc2020" style={{textAlign:"center",color:"white"}}>2020</h1>
-
-			<article className="container mcworldcontainer">
-				<section>
-					
-				</section>
-				<section>
-					<h2>Parasite Server (August 2020)</h2>
-					<ul>
-						<li>Server Owner: inkvoiid</li>
-						<li>Date Started: August 2020</li>
-						<li>Date Ended: September 2020</li>
-					</ul>
-				</section>
-			</article>
-
-			<h1 id="mc2021" style={{textAlign:"center",color:"white"}}>2021</h1>
-
-			<h1 id="mc2022" style={{textAlign:"center",color:"white"}}>2022</h1>
-
-			<h1 id="mc2023" style={{textAlign:"center",color:"white"}}>2023</h1>
-
-			<div className="container">
-				
-				<Panorama src={DamonBase} />
-				<Panorama src={ZachHouse} />
-			</div>
-		</ScrollSpy>
+        {yearHeadings.map((year) => (
+          <div key={year}>
+            <h1
+              id={`mc${year}`}
+              className="underline"
+              style={{ textAlign: "center", color: "white", textDecorationColor: "steelblue" }}
+            >
+              {year}
+            </h1>
+            {serversByYear[year].map((server, idx) => (
+              <ServerEntry key={idx} server={server} />
+            ))}
+          </div>
+        ))}
+      </ScrollSpy>
 	</main>
 	  );
 }
